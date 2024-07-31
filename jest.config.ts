3fc12b2a7bae295b7871521eddd02812aa9028d8
Ali Amori Kadhim <ali@frameless.io>
2024-07-31 09:53:28 +0200

    test: integrate Jest & react-testing-library into React app

---
+++ jest.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  moduleNameMapper: {
+    '\\.(css|scss)$': 'identity-obj-proxy',
+    "^.+\\.svg": "<rootDir>/tests/mocks/svgMock.tsx"
+  },
+  setupFilesAfterEnv: ['<rootDir>/tests/setupTests.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  modulePaths: ['<rootDir>'],
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', {
+      tsconfig: 'tsconfig.test.json',
+    }],
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+};
+
+export default config;
